951d0dbbe7d2e0dc1602abafe11d7a401807a98c
thiago <thiago.bertachini@hotmail.com>
2025-06-17 17:27:47 -0300

    new files and submodule

---
+++ src/main/java/com/algaworks/algasensors/temperature/processsing/TemperatureProcesssingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.temperature.processsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcesssingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureProcesssingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algasensors/temperature/processsing/TemperatureProcesssingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.temperature.processsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcesssingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
