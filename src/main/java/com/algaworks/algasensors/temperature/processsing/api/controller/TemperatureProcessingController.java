2343f8a913a7eb46b4cb05140c70b19c1aab91d2
thiago <thiago.bertachini@hotmail.com>
2025-07-22 15:23:19 -0300

    feat: producing temperature value

--- src/main/java/com/algaworks/algasensors/temperature/processsing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/algaworks/algasensors/temperature/processsing/api/controller/TemperatureProcessingController.java
@@ -3,8 +3,10 @@ package com.algaworks.algasensors.temperature.processsing.api.controller;
 import com.algaworks.algasensors.temperature.processsing.api.model.TemperatureLogOutput;
 import com.algaworks.algasensors.temperature.processsing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.flogger.Flogger;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +14,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
+import static com.algaworks.algasensors.temperature.processsing.infrastructure.rabbitmq.RabbitMQConfig.TEMPERATURE_PROCESSSING_EXCHANGE;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
@@ -38,5 +45,14 @@ public class TemperatureProcessingController {
                 .value(temperature)
                 .build();
         log.info("Received temperature: {}", logOutput.toString());
+
+        log.info("Sending temperature: {}", logOutput);
+        String routingKey = "";
+
+        rabbitTemplate.convertAndSend(TEMPERATURE_PROCESSSING_EXCHANGE, routingKey, logOutput,
+                message -> {
+                    message.getMessageProperties().setHeader("sensorId", sensorId.toString());
+                    return message;
+                });
     }
 }
